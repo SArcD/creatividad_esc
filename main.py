5dbdd3b23b794afa92bb1df4baefe4be7a59c955
SArcD <141703135+SArcD@users.noreply.github.com>
2025-06-11 20:41:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,6 @@ import pandas as pd
 import numpy as np
 import datetime
 
-#st.set_page_config(page_title="Escala de Habilidades Sociales de Gismero", layout="centered")
 
 st.title("🗣️ Escala de Habilidades Sociales (EHS) - Gismero 2010")
 
